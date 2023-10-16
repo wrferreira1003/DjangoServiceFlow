6dccb2052be5163302bd61a2c280715da56247de
wrferreira1003 <wrf.wellington@gmail.com>
2023-10-16 15:33:13 -0300

    Ajuste Channels

--- ServidorScred/asgi.py
+++ ServidorScred/asgi.py
@@ -1,16 +1,18 @@
-"""
-ASGI config for ServidorScred project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import Cliente.routing
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ServidorScred.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            Cliente.routing.websocket_urlpatterns
+        )
+    ),
+})
